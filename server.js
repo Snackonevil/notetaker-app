9cd3fa6a955dfc4d64eee16df39168091a907266
Kevin Lacson <lacsonky@gmail.com>
2022-01-15 21:57:41 -0500

    connected api

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const noteData = require('./db/db.json');
+const uuid = require('uuid');
 
 const app = express();
+app.use(express.static('public'));
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,5 +16,7 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+
+app.get('/api/notes', (req, res) => res.json(noteData));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
