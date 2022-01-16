9ee0f72ba6450cbe3f6f9a71e05f3b2e1218527a
Kevin Lacson <lacsonky@gmail.com>
2022-01-15 21:30:23 -0500

    built server base and reordered directories

--- server.js
+++ server.js
@@ -3,10 +3,14 @@ const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
